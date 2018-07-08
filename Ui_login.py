ad15a8e9fff89bdf67ebe08fdbd6d05ca9bbea20
kiorry <kiorry@163.com>
2018-07-08 13:26:36 +0800

    第六次修改

--- Ui_login.py
+++ Ui_login.py
@@ -9,13 +9,25 @@ import userDB
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(400, 300)
+
+        # 设置背景图片
+        # palette1 = QtGui.QPalette()
+        # palette1.setColor(Form.backgroundRole(), QColor(192, 253, 123))
+        # palette1.setBrush(Form.backgroundRole(), QtGui.QBrush(QtGui.QPixmap('./BG.png')))
+        # Form.setPalette(palette1)
+
+
         self.verticalLayout = QtWidgets.QVBoxLayout(Form)
         self.verticalLayout.setObjectName("verticalLayout")
+
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
@@ -47,11 +59,12 @@ class Ui_Form(object):
         spacerItem3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_2.addItem(spacerItem3)
         self.verticalLayout.addLayout(self.horizontalLayout_2)
+
         self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_3.setObjectName("horizontalLayout_3")
         spacerItem4 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-
         self.horizontalLayout_3.addItem(spacerItem4)
+
         self.pushButton_submit = QtWidgets.QPushButton(Form)
         self.pushButton_submit.setObjectName("pushButton_submit")
         self.horizontalLayout_3.addWidget(self.pushButton_submit)
@@ -75,29 +88,22 @@ class Ui_Form(object):
         name, password = userDB.searchDB()
         input_account = self.lineEdit_account.text()
         input_password = self.lineEdit_password.text()
-        # print("帐号:%s"%account)
-        # print("密码:%s"%passoword)
+
         if input_account == name and input_password == password:
             self.lineEdit_account.clear()
             self.lineEdit_password.clear()
             print("well doen!")
+
         else:
             # print("请输入正确的用户名及密码!")
             self.lineEdit_account.clear()
             self.lineEdit_password.clear()
             msg = QtWidgets.QMessageBox()
             msg.critical(Form,"帐号密码输入错误!","请输入正确的帐号及密码!")
-            # print(dialog)
-    def ShowErrorDialog(self):
-        dialog = QtWidgets.QMessageBox.critical()
-        print(dialog)
-        # dialog.setWindowTitle("MessageError")
-        # dialog.exec_()
+
 
 if __name__ == "__main__":
     import sys
-
-
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
     ui = Ui_Form()
